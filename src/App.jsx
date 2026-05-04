0f5bc7d30c7c9ce3c07f1932132b03051b994053
missbastimi <salimabastimi@gmail.com>
2026-05-04 21:44:10 +0000

    feat: quiz page fuctionality, restart quiz functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Index.css';
 import Quiz from './components/quiz';
-
+import Results from './components/results';
 function App() {
   return (
     <div>
--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,47 +1,96 @@
 import React, { useState } from 'react';
+import Results from './results';
+
 const questionBank = [
   {
     question: 'Who is the president of Ghana?',
     options: ['John Dramani Mahama', 'Nana Addo Danquah'],
     answer: 'John Dramani Mahama',
+    id: 1,
   },
   {
-    question: 'Who is the president of Ghana?',
-    options: ['John Dramani Mahama', 'Nana Addo Danquah'],
-    answer: 'John Dramani Mahama',
+    question: 'HTML + JS =',
+    options: ['XML', 'JSX'],
+    answer: 'JSX',
+    id: 2,
   },
   {
-    question: 'Who is the president of Ghana?',
-    options: ['John Dramani Mahama', 'Nana Addo Danquah'],
-    answer: 'John Dramani Mahama',
+    question: '5+3=',
+    options: ['53', '8'],
+    answer: '8',
+    id: 3,
   },
 ];
 
-const initialAnswers = ['null', 'null', 'null'];
-
 function Quiz() {
+  const initialAnswers = [null, null, null];
   const [userAnswers, setUserAnswers] = useState(initialAnswers);
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
 
-  function handleSelectOption(option) {}
+  const [isQuizFinished, setIsQuizFinished] = useState(false);
+
+  const selectedAnswer = userAnswers[currentQuestion];
+
+  function handleSelectOption(option) {
+    const newUserAnswers = [...userAnswers];
+    newUserAnswers[currentQuestion] = option;
+
+    setUserAnswers(newUserAnswers);
+  }
+
+  function goToNext() {
+    if (currentQuestion === questionBank.length - 1) {
+      setIsQuizFinished(true);
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+  }
+  function goToPrev() {
+    if (currentQuestion > 0) {
+      setCurrentQuestion(currentQuestion - 1);
+    }
+  }
+
+  function restartQuiz() {
+    setUserAnswers(initialAnswers);
+    setCurrentQuestion(0);
+    setIsQuizFinished(false);
+  }
+
+  if (isQuizFinished) {
+    return (
+      <Results
+        userAnswers={userAnswers}
+        questionBank={questionBank}
+        restartQuiz={restartQuiz}
+      />
+    );
+  }
 
   return (
     <div>
-      <h2>Question 1</h2>
-      <p className="question">{questionBank[0].question}</p>
+      <h2>Question {currentQuestion + 1}</h2>
 
-      {questionBank[0].options.map((option) => (
-        <button className="option" onClick={() => handleSelectOption(option)}>
+      <p className="question">{questionBank[currentQuestion].question}</p>
+
+      {questionBank[currentQuestion].options.map((option) => (
+        <button
+          key={questionBank.id}
+          className={'option' + (selectedAnswer === option ? 'selected' : '')}
+          onClick={() => handleSelectOption(option)}
+        >
           {option}
         </button>
       ))}
 
-      <p>Option Selected = {optionSelected}</p>
-
       <div className="nav-buttons">
-        <button>Previous</button>
-        <button>Next</button>
+        <button onClick={goToPrev} disabled={currentQuestion === 0}>
+          Previous
+        </button>
+        <button onClick={goToNext} disabled={!selectedAnswer}>
+          {currentQuestion === questionBank.length - 1 ? 'finish quiz' : 'next'}
+        </button>
       </div>
     </div>
   );
